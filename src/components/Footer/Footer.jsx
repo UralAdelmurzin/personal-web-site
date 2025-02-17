f97645d29b9eac0eb5ccea5b27abc7ee68a55a45
Kostromichov Roman <kostromichov.roman@gmail.com>
2025-02-17 22:24:19 +0200

    replace logo with TikTok icon in footer and update contact email

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,9 +3,9 @@ import './style.scss'
 
 // import clock from 'src/assets/img/widget-clock.svg'
 // import address from 'src/assets/img/widget-address.svg'
-// import mail from 'src/assets/img/widget-mail.svg'
+import mail from 'src/assets/img/widget-mail.svg'
 import phone from 'src/assets/img/widget-phone.svg'
-// import logo from 'src/assets/img/logo-landing.png'
+import tiktok from 'src/assets/img/tiktok-icon.svg'
 import { contacts } from 'src/utils/datasets';
 
 
@@ -14,35 +14,36 @@ function Footer() {
     <div className='landing_footer'>
       <div className="container">
         <div className="footer_body">
-          {/* <div className='footer_widget'>
-            <img src={clock} alt="icon" />
-            <h3 className='widget_title'>Working Hours:</h3>
-            <p className='widget_text'>On weekdays from 11:00 AM to 8:00 PM</p>
-            <p className='widget_text'>Saturday - Closed</p>
-            <p className='widget_text'>Sunday - Closed</p>
-          </div> */}
+
           <div className='footer_widget'>
             <img src={phone} alt="icon" />
             <h3 className='widget_title'>Phone numbers:</h3>
-            <p className='widget_text'>{ contacts.phoneNumber }</p>
+            <p className='widget_text'><a href={`tel:${contacts.phoneNumber}`}>{ contacts.phoneNumber }</a></p>
             {/* <p className='widget_text'>{ contacts.phoneNumber_2 }</p> */}
           </div>
-        </div>
 
-        {/* <div className="footer_body">
           <div className='footer_widget'>
             <img className='icon' src={mail} alt="icon" />
             <h3 className='widget_title'>Email:</h3>
-            <p className='widget_text'>{ contacts.email }</p>
+            <p className='widget_text'><a href={`mailto:${contacts.email}`}>{ contacts.email }</a></p>
           </div>
+        </div>
+
+        <div className="footer_body">
           <div className='footer_widget'>
+            <img src={tiktok} alt="icon" />
+            <h3 className='widget_title'>TikTok:</h3>
+            <p className='widget_text'><a href={contacts.tiktokUrl} target='_blank'>{ contacts.tiktokName }</a></p>
+          </div>
+
+          {/* <div className='footer_widget'>
             <img src={address} alt="icon" />
             <h3 className='widget_title'>Address:</h3>
             <p className='widget_text'>{ contacts.street }</p>
             <p className='widget_text'>{ contacts.city }</p>
             <p className='widget_text'>{ contacts.country }</p>
-          </div>
-        </div> */}
+          </div> */}
+        </div>
       </div>
 
       <div className="footer_bottom">
--- src/utils/datasets.js
+++ src/utils/datasets.js
@@ -1,13 +1,16 @@
 export const contacts = {
   phoneNumber: "(818) 262-4142",
   phoneNumber_2: "+1-877-000-2222",
-  email: 'sample@gmail.com',
+  email: 'adelmurza777@gmail.com',
 
   street: '000 Garnet St',
   city: "Los Angeles",
   country: 'USA',
 
   siteName: 'smaple.com',
+
+  tiktokUrl: 'https://www.tiktok.com/@uraladel',
+  tiktokName: '@uraladel',
 }
 
 export const menuList = [
